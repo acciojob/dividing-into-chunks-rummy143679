6e85fd3848e0e91185146e755c0be947643e63c2
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-12 22:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const re = [];
+		let start = 0;
+	for(let i=0;i<arr.length;i++){
+		const sr = arr.slice(i,i+1)
+		const sum = sr.reduce((a,e) => {
+			return a+e
+		},0)
+		if(sum <= 5){
+			re.push(arr.slice(start, i))
+			start = i+1;
+		}else if(sum > 5){
+			re.push(arr.slice(start, i-1))
+			start = i;
+		}
+	}
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
